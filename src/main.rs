15230fa493eff9f955edf1bcf8f50371ae4b39cc
mdsboy <modasokun@gmail.com>
2019-12-01 19:24:20 +0900

    ルートディレクトリのみls実装

--- src/main.rs
+++ src/main.rs
@@ -38,10 +38,8 @@ fn main() {
         stat.st_blksize as usize
     };
     println!("{}", img_size);
-    
     let img = unsafe { MmapOptions::new().map(&file).unwrap() };
     println!("{:?}", img);
-    
     let sblk = get_superblock(&img);
     println!("{:?}", sblk);
 
@@ -51,7 +49,7 @@ fn main() {
     match &**cmd {
         "ls" => {
             println!("ls");
-            do_ls(&img, args.len() - 3, &args[3..]);
+            do_ls(&img, &root_inode, args.len() - 3, &args[3..]);
         }
         _ => {
             unimplemented!();
@@ -115,11 +113,11 @@ fn get_superblock(img: &memmap::Mmap) -> SuperBlock {
 const IPB: usize = BSIZE / mem::size_of::<Dinode>();
 
 fn iget(img: &memmap::Mmap, sblk: &SuperBlock, inum: usize) -> Dinode {
-    println!("IPB:{}", IPB);
+    //println!("IPB:{}", IPB);
     let pos = inum / IPB + sblk.inodestart as usize;
-    println!("{}", pos);
+    //println!("{}", pos);
     let offset = (inum % IPB) as u8;
-    println!("{}", offset);
+    //println!("{}", offset);
 
     let inode_pos = BSIZE * pos + mem::size_of::<Dinode>();
 
@@ -144,16 +142,113 @@ fn iget(img: &memmap::Mmap, sblk: &SuperBlock, inum: usize) -> Dinode {
             img[inode_pos + 12 + i * 4 + 0],
         ]);
     }
-    println!("{:?}", root_inode);
+    //println!("{:?}", root_inode);
     return root_inode;
 }
 
-fn do_ls(img: &memmap::Mmap, argc: usize, argv: &[String]) {
+const DIRSIZ: usize = 14;
+/*
+fn skiplem(path: &String, name: &String) -> String {
+    let mut i = 0;
+    let path: Vec<char> = path.chars().collect();
+    while path[i] == '/' {
+        i += 1;
+    }
+    path
+}
+*/
+
+/*
+
+fn dlookup(img: &memmap::Mmap, dp: &Dinode, name: &String, offp: u32) -> Option<Dinode> {
+    let mut off = 0;
+    while off < dp.size {
+        off += mem::size_of::<Dinode>() as u32;
+        let de =
+        if name == de.name {}
+    }
+    None
+}*/
+/*
+fn bmap(img: &memmap::Mmap, ip: &Dinode, n: usize) {
+    if n < NDIRECT {
+        let addr = ip.addrs[n];
+        if addr == 0 {
+            addr =
+        }
+    }
+}*/
+
+fn iread(img: &memmap::Mmap, ip: &Dinode, n: usize, off: usize) -> Vec<u8> {
+    /*let mut t = 0;
+    let mut m = 0;
+    let mut off = off;*/
+    //while t < n {
+    let mut buf = vec![0; n];
+    (&img[BSIZE * ip.addrs[(off / BSIZE) as usize] as usize + off % BSIZE..])
+        .read_exact(buf.as_mut_slice())
+        .unwrap();
+/*
+    let po = ip.addrs[(off / BSIZE) as usize] as usize;
+    println!("addr:{}", po);
+    println!("off:{}", off);
+    println!("{:?}", img[po..po+10].to_vec());*/
+    /*m = std::cmp::min(n - t, BSIZE - off % BSIZE);
+        t += m;
+        off += m;
+    }
+    vec![]*/
+    buf
+}
+
+fn ilookup(img: &memmap::Mmap, rp: &Dinode, path: &String) -> Dinode {
+    let mut name = ['\0'; DIRSIZ + 1];
+    iget(img, &get_superblock(img), 1)
+}
+
+const T_DIR: i16 = 1;
+
+fn do_ls(img: &memmap::Mmap, root_inode: &Dinode, argc: usize, argv: &[String]) {
     if argc != 1 {
         println!("error");
         return;
     }
     let path = &argv[0];
+    let ip = ilookup(img, root_inode, &path);
+    if ip.file_type == T_DIR {
+        let mut off = 0;
+        while off < ip.size {
+            //let mut buf = vec![0; std::mem::size_of::<Dinode>()];
+            let buf = iread(&img, &ip, std::mem::size_of::<Dirent>(), off as usize);
+            //println!("{:?}", buf);
+
+            let mut de = Dirent {
+                inum: u16::from_be_bytes([buf[1], buf[0]]),
+                name: [0; DIRSIZ],
+            };
+
+            for i in 0..DIRSIZ {
+                de.name[i] = buf[i + 1];
+            }
+
+            off += std::mem::size_of::<Dirent>() as u32;
+            if de.inum == 0 {
+                continue;
+            }
+
+            let p = iget(img, &get_superblock(img), de.inum as usize);
+            println!(
+                "{}, {}, {}, {}",
+                de.name.iter().map(|&c| c as char).collect::<String>(),
+                p.file_type,
+                de.inum,
+                p.size
+            );
+
+            //println!("{}", off);
+        }
+    } else {
+    }
 }
 
 #[derive(Debug)]
@@ -180,4 +275,10 @@ struct Dinode {
     nlink: i16,                // Number of links to inode in file system
     size: u32,                 // Size of file (bytes)
     addrs: [u32; NDIRECT + 1], // Data block addresses
+}
+
+#[derive(Debug)]
+struct Dirent {
+    inum: u16,
+    name: [u8; DIRSIZ],
 }
